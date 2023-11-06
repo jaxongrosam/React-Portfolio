54042da41f42eaaee5c28663a8708588a4eb4861
jaxon grosam <jaxon.grosam@gmail.com>
2023-11-06 12:16:37 -0600

    page is running and displaying content

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
